d6e7326aaa39b24922fa3623952cf516e5d6d529
dmitry19942 <gorustovich2013@gmail.com>
2023-02-28 01:21:18 +0300

    hw12 fix

--- src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
+++ src/s2-homeworks/hw07/common/c5-SuperSelect/SuperSelect.tsx
@@ -37,7 +37,8 @@ const SuperSelect: React.FC<SuperSelectPropsType> = ({
 
     const onChangeCallback = (e: ChangeEvent<HTMLSelectElement>) => {
         // делают студенты
-        onChangeOption?.(+e.currentTarget.value)
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(+e.currentTarget.value)
     }
 
     const finalSelectClassName = s.select + (className ? ' ' + className : '')
--- src/s2-homeworks/hw12/bll/themeReducer.ts
+++ src/s2-homeworks/hw12/bll/themeReducer.ts
@@ -8,17 +8,17 @@ type InitStateType = {
 
 type ChangeThemeIdType = {
     type: 'SET_THEME_ID',
-    themeId: number
+    id: number
 }
 
 export const themeReducer = (state: InitStateType = initState, action: ChangeThemeIdType): InitStateType => { // fix any
     switch (action.type) {
         // дописать
         case "SET_THEME_ID":
-        return {...state, themeId: action.themeId}
+        return {...state, themeId: action.id}
         default:
             return state
     }
 }
 
-export const changeThemeId = (themeId: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', themeId }) // fix any
+export const changeThemeId = (id: number): ChangeThemeIdType => ({ type: 'SET_THEME_ID', id }) // fix any
